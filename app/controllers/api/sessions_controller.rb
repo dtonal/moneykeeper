2e0502294d51822399204cfdd170effa215decdc
dtonal <dtonal@posteo.de>
2016-10-02 18:34:06 +0200

    Change the sessions post response to valid json response

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,11 @@
 module Api
   class SessionsController < Api::OpenBaseController
     def create
-      user = User.find_by_email(params[:email])
+      @user = User.find_by_email(params[:email])
       # If the user exists AND the password entered is correct.
-      if user && user.authenticate(params[:password])
-        render json: user.auth_token
+      if @user && @user.authenticate(params[:password])
+        output = {'token' => @user.auth_token}.to_json
+        render json: output
       else
         render nothing: true, status: :bad_request
       end
